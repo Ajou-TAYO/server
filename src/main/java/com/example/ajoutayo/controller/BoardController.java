24adc3b8c82b4f5043c74ffaf94af1fb2f9e570d
tndls2 <tndls3026@ajou.ac.kr>
2023-04-03 16:44:57 +0900

    feat: Set response message, Refactory board response

--- src/main/java/com/example/ajoutayo/controller/BoardController.java
+++ src/main/java/com/example/ajoutayo/controller/BoardController.java
@@ -3,11 +3,15 @@ package com.example.ajoutayo.controller;
 import java.util.List;
 
 import com.example.ajoutayo.domain.Board;
+import com.example.ajoutayo.dto.StatusCode;
 import com.example.ajoutayo.dto.request.BoardCreateDto;
 import com.example.ajoutayo.dto.request.BoardUpdateDto;
 import com.example.ajoutayo.dto.response.BoardResponseDto;
+import com.example.ajoutayo.dto.response.DefaultResponse;
+import com.example.ajoutayo.dto.response.ResponseMessage;
 import com.example.ajoutayo.service.BoardService;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
@@ -21,25 +25,32 @@ import javax.servlet.http.HttpServletResponse;
 public class BoardController {
     private final BoardService boardService;
     @GetMapping("/{id}")
-    public BoardResponseDto getBoard(@PathVariable("id") Long id, HttpServletRequest request, HttpServletResponse response){
+    public ResponseEntity<?> getBoard(@PathVariable("id") Long id, HttpServletRequest request, HttpServletResponse response){
         boardService.viewCount(id, request, response); // views ++
-        return boardService.getBoard(id);
+        BoardResponseDto board= boardService.getBoard(id);
+        return ResponseEntity.ok(DefaultResponse.res(StatusCode.OK, ResponseMessage.GET_BOARD,board));
     }
     @DeleteMapping("/{id}")
-    public void deleteBoard(@PathVariable("id") Long id){
+    public ResponseEntity<?> deleteBoard(@PathVariable("id") Long id){
         boardService.deleteBoard(id);
+        return ResponseEntity.ok(DefaultResponse.res(StatusCode.OK, ResponseMessage.DELETE_BOARD));
     }
     @PatchMapping("/{id}")
-    public BoardResponseDto updateBoard(@PathVariable("id") Long id, @RequestBody BoardUpdateDto boardUpdateDto){
-        return boardService.updateBoard(id, boardUpdateDto);
+    public ResponseEntity<?> updateBoard(@PathVariable("id") Long id, @RequestBody BoardUpdateDto boardUpdateDto){
+        BoardResponseDto board= boardService.updateBoard(id, boardUpdateDto);
+        return ResponseEntity.ok(DefaultResponse.res(StatusCode.OK, ResponseMessage.UPDATE_BOARD,board));
     }
     @PostMapping("/new")
-    public Long createBoard(@RequestBody BoardCreateDto boardCreateDto) {
-        return boardService.saveBoard(boardCreateDto);
+    public ResponseEntity<?> createBoard(@RequestBody BoardCreateDto boardCreateDto) {
+        Long boardId = boardService.saveBoard(boardCreateDto);
+        BoardResponseDto board= boardService.getBoard(boardId);
+        return ResponseEntity.ok(DefaultResponse.res(StatusCode.OK, ResponseMessage.CREATE_BOARD,board));
+
     }
     @GetMapping("")
-    public List<Board> getAllBoards() {
-        return boardService.getAllBoards();
+    public ResponseEntity<?>  getAllBoards() {
+        List<Board> boardList = boardService.getAllBoards();
+        return ResponseEntity.ok(DefaultResponse.res(StatusCode.OK, ResponseMessage.GET_ALL_BOARD, boardList));
     }
 
 }
