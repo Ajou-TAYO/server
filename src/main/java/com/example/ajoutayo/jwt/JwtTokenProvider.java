e5a5b7fce747a11b3ba580e722509e6e6545307b
suin park <tndls3026@ajou.ac.kr>
2023-05-28 20:59:44 +0900

    fix: Extend jwt tkoen expire

--- src/main/java/com/example/ajoutayo/jwt/JwtTokenProvider.java
+++ src/main/java/com/example/ajoutayo/jwt/JwtTokenProvider.java
@@ -28,8 +28,8 @@ public class JwtTokenProvider {
 
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "Bearer ";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            // 30분
-    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 24 * 7;            // 7일
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 14;  // 14일
     public JwtTokenProvider(@Value("${jwt.secret}") String secretKey) {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         this.key = Keys.hmacShaKeyFor(keyBytes);
