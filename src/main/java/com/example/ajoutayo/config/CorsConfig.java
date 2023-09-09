0eeb8c7d442078ab93a4dd8d79a8cbd16da7ae79
suin park <tndls3026@ajou.ac.kr>
2023-09-10 08:12:47 +0900

    hotfix: cors 에러-> 허용 Origin 추가

--- src/main/java/com/example/ajoutayo/config/CorsConfig.java
+++ src/main/java/com/example/ajoutayo/config/CorsConfig.java
@@ -13,6 +13,8 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://121.137.66.90:5173");
+        config.addAllowedOriginPattern("localhost:3000");
         config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
